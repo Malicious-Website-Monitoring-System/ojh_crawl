dd0d98cdfd67d35ead46488a5c1456996c4bf47f
Ohjunghh <20200685@duksung.ac.kr>
2024-05-27 20:14:58 +0900

    update selenium_keywords

--- crawler/selenium_collect.py
+++ crawler/selenium_collect.py
@@ -42,3 +42,14 @@ def extract_links(start_url, url_queue, collected_starturl, collected_urls) : #,
 
     print("Processing complete for:", start_url)
 
+
+# is_dynamic_content 체크:
+
+# is_dynamic 변수를 통해 페이지가 동적인지 확인.
+# 자바스크립트 태그가 많은 페이지는 동적 콘텐츠로 간주.
+# BeautifulSoup과 Selenium 병행 사용:
+
+# 페이지가 동적 콘텐츠를 포함한다고 판단되면 Selenium을 사용하여 페이지를 로드.
+# 그렇지 않은 경우 BeautifulSoup을 사용하여 페이지를 로드.
+# 이 방식으로, 동적 콘텐츠가 필요하지 않은 페이지는 BeautifulSoup을 사용하여 더 빠르게 처리하고
+# 동적 콘텐츠가 필요한 페이지만 Selenium을 사용하여 처리함으로써 전체 크롤링 작업의 효율성을 높일 수 있습니다.
